e21965eeb8a3c174a47eec1c4954c49114dce2db
Saurav kumar jha <skjsaurav1@gmail.com>
2024-12-03 15:20:39 +0530

    deployment

--- src/main/java/com/protofolio/Client/data/ClientDataApiApplication.java
+++ src/main/java/com/protofolio/Client/data/ClientDataApiApplication.java
@@ -9,5 +9,4 @@ public class ClientDataApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ClientDataApiApplication.class, args);
 	}
-
 }
--- src/main/java/com/protofolio/Client/data/config/corsConfig.java
+++ src/main/java/com/protofolio/Client/data/config/corsConfig.java
@@ -1,5 +1,6 @@
 package com.protofolio.Client.data.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -7,13 +8,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 @EnableWebMvc
 public class corsConfig implements WebMvcConfigurer{
-
+    @Value(value = "${frontend_url}")
+    private String frontend_url;
+    
     @Override
     public void addCorsMappings(CorsRegistry corsRegistry){
 
         corsRegistry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173/")
+                .allowedOrigins(frontend_url)
                 .allowedMethods("GET","POST","PUT","DELETE")
+                .allowedHeaders("*")                
                 .maxAge(3600);
     }
 }
